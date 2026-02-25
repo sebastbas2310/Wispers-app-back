588b179e7cd3e08fbd97cde38380bedad464eb8e
sebastian bermudez buritica <sbermudezburitica@gmail.com>
2026-02-25 14:04:50 -0500

    Echo CRUDS fixed

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ¡Aquí sucede la magia!
+  app.enableCors({
+    origin: 'https://wispers-app.vercel.app', // Tu frontend en Vercel
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
